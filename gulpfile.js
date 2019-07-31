5a6c5918f723884cba986432a6ac1d62e3ca5aa8
Streefkerk,Tobias T.S <t.streefkerk@student.fontys.nl>
2019-07-31 09:39:52 +0200

    Added SASS support and reworked project as node instead of group

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+//gulpfile.js
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// Define tasks after requiring dependencies
+function style() {
+    // Where should gulp look for the sass files?
+    // My .sass files are stored in the styles folder
+    // (If you want to use scss files, simply look for *.scss files instead)
+    return (
+        gulp
+            .src("sites/all/themes/comport/sass/style.scss")
+
+            // Use sass with the files found, and log any errors
+            .pipe(sass())
+            .on("error", sass.logError)
+
+            // What is the destination for the compiled file?
+            .pipe(gulp.dest("sites/all/themes/comport/css"))
+    );
+}
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
+
+function watch(){
+    // gulp.watch takes in the location of the files to watch for changes
+    // and the name of the function we want to run on change
+    gulp.watch('sites/all/themes/comport/sass/style.scss', style)
+}
+
+// Don't forget to expose the task!
+exports.watch = watch;
+
+

146afad1ea10d6e2399a4aaa9aabab6cd7e84089
Streefkerk,Tobias T.S <t.streefkerk@student.fontys.nl>
2019-08-13 17:09:03 +0200

    Submodules

--- sites/all/themes/enorm/template.php
+++ sites/all/themes/enorm/template.php
@@ -11,8 +11,8 @@
 /*
     Code was here for testing purposes, do not turn on or remove.
 */
-//function enorm_theme($existing, $type, $theme, $path) {
-//    $items['enorm_company_search_form'] = array(
+//function enorm_theme_theme($existing, $type, $theme, $path) {
+//    $items['enorm_theme_company_search_form'] = array(
 //        'render element' => 'form',
 //        'template' => 'company-search',
 //        'path' => drupal_get_path('theme', 'enorm') . '/templates',

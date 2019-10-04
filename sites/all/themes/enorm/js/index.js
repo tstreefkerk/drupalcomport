cb4e3b87a598de037b563191c86012b6081f0025
Streefkerk,Tobias T.S <t.streefkerk@student.fontys.nl>
2019-10-04 09:40:33 +0200

    Node form styling

--- sites/all/themes/enorm/js/index.js
+++ sites/all/themes/enorm/js/index.js
@@ -10,9 +10,9 @@
             $(".field-name-field-project-description").after("<div id='line-break'></div>");
             $(".field-name-field-callback-request-done").after("<div id='line-break'></div>");
             $(".project-expiring").prependTo("#node-content-type-project-detail");
-            $(".views-field-title a").click(function (e) {
+            $(".page-all-projects-customer .views-field-title a").click(function (e) {
                 $(".col-sm-12").fadeOut();
-                $(".row").addClass('enorm-spinner-container').html("<img src='/sites/all/themes/enorm/assets/img/logo-enormkopie.gif' class='enorm-spinner'><br><p>Loading...</p>");
+                $(".row").addClass('enorm-spinner-container').html("<img src='/sites/all/themes/enorm/assets/img/enorm-loader-transparent.gif' class='enorm-spinner'><br><p>Loading...</p>");
             });
             $(".view-header").once('enorm').click(function () {
                 if ($(this).next(".view-content").hasClass("vanishIn")) {
